5c4c358a0506ebb5aa2481653bb34a08ff79d654
Aivon99 <109174292+Aivon99@users.noreply.github.com>
2024-08-20 00:29:58 +0200

    Cambiamenti in esercizioEfficienza e ExerciseManager, principalmente cambio ordini di istruzioni/add(). inibito metodo in finestraEsercizio che inivìbiva i figli di layout. Rimane da ordinare i metodi/stringe verticalmente invece che orizzontalmente e fare un po' di housework

--- Esercizio2.java
+++ Esercizio2.java
@@ -1,136 +1,48 @@
-/*
- * After some tries time complexity came outtoo high, next will try a different approach with dinamic programming
- Also issues with reused variables etc, code is horrible, comments missing, names are ugly
- */
+import java.util.*;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.ArrayList;
-import java.util.Iterator;
+class Nodo {                /// non tutti i prefissi sono condivisi da tutte le lettere Es 00 permette B,C,E,H ma preclude D --> non ha senso controllare sempre tutti ---> faccio un albero 
 
-
-class Decodificatore{
-    private  StringBuilder combinazione;
-    private Queue<Character> arrayCod;
-
-    public Decodificatore(){ //Con campi vuoti
-        arrayCod = new LinkedList<>();
-        this.combinazione = new StringBuilder();
-    }
-
-    public Decodificatore( char[] Input){
-        arrayCod = new LinkedList<>(); //Lo uso come FIFO, aggiungo tutti i valori, in seguito man mano che il codice binario viene interpretato li rimuovo 
-        for(char c : Input){
-            this.arrayCod.add(c);
-        }
-        this.combinazione = new StringBuilder(); ///Man mano che interpreto il codice aggiungo le lettere allo StringBuilder;
-        
-    }
-    public Decodificatore(Decodificatore other) {
-        this.combinazione = new StringBuilder(other.combinazione);
-        this.arrayCod = new LinkedList<>(other.arrayCod);
-    }
-    public void setCombinazione(String lettere){
-        this.combinazione.setLength(0); //Svuoto il Stringbuilder precedente
-        this.combinazione.append(lettere); //Aggiungo le lettere 
-    }
-    public void setCodice(char[] codice){
-        arrayCod.clear(); //Svuoto la coda
-        for(int i = 0; i < codice.length; i++){
-            arrayCod.add(codice[i]); //Aggiungo nuovo codice
-        }
-
-    }
-    public char[] getCod(){ 
-        Character[] arrayCharacter = arrayCod.toArray(new Character[0]);
-
-            char[] charArray = new char[arrayCharacter.length];
-        for (int i = 0; i < arrayCharacter.length; i++) {
-            charArray[i] = arrayCharacter[i]; // Unboxing del
-        }    
-        return charArray;
-    }
-    public void printCombinazione(){
-        System.out.println();
-       System.out.print(combinazione.toString());
-    }
-    public void addLettera(char lettera){
-        combinazione.append(lettera);
-    }
-    public String getLetter(){
-        return  combinazione.toString();
-    }
-    public void rimCodice(int n){   //metodo che dato un elemento char[] "es", elimina i primi es.length char dal codice   
-    for(int i = 0; i < n; i++){
-        this.arrayCod.poll();
-    }    
+    Map<Character, Nodo> children = new HashMap<>();
+    String lettera = null; 
 }
-    public boolean match(char[] lettera){ //metodo che verifica se i caratteri in cima alla coda corrispondono a quelli del codice della lettera
-        Iterator<Character> iteratore = this.arrayCod.iterator(); //Definisco un iteratore per parsare la coda senza modificarla
-        for(int i = 0; i < lettera.length;i++){
-            if(iteratore.hasNext()){
-                if(iteratore.next() != lettera[i] ){
-                    return false;
-                }
-            }
-            else return false; //Qualora la parola finisse prima della lettera ritorno falso. 
+
+class Albero {
+    Nodo radice = new Nodo();
+    
+
+    public void inserisci(String code, String lettera){   //Inserichi codice e relativa lettera, la lettera viene posta come foglia nel albero
+        Nodo nodo = radice;
+        for (char c : code.toCharArray()) { //Il codice binario è dato come stringa, viene convertito a char[], e aggiunto all'albero 
+            nodo.children.putIfAbsent(c, new Nodo()); //qualora non esistesse già, lega al nodo un nuovo nodo corrispondente al valore c
+            nodo = nodo.children.get(c); //in ogni caso prosegue prendendo tra i figli del nodo quello corrispondente al char c preso in esame 
         }
-        return true;
+        nodo.lettera = lettera; //alla fine del loop (quindi quando il codice binario è finito) aggiunge valore al parametro lettera
     }
-    public int getLunghezzaCodice(){
-        return this.arrayCod.size();
-    } 
+
+    public Nodo getRadice() {
+        return radice;
+    }
 }
 
 public class Esercizio2 {
     public static void main(String[] args) {
-        char[][] Lettere = {
-            {'0'}, {'0', '0'}, {'0', '0', '1'}, {'0', '1', '0'},
-            {'0', '0', '1', '0'}, {'0', '1', '0', '0'}, {'0', '1', '1', '0'}, {'0', '0', '0', '1'}
-        };
-        char[] lettereChar = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
-        
-        char[] arrayCodice = leggiFile("args[0]"); //leggo il file con gli input
-        
-        Queue<Decodificatore> Albero = new LinkedList<>();  
-            Decodificatore origine = new Decodificatore(arrayCodice);
-        Albero.add(origine); //Inizializzo la coda
-        ArrayList<String> Iterazioni = new ArrayList<>(); //per tenere le iterazioni valide
-        
-        Decodificatore elemento = Albero.poll(); 
-        do{
-            for(int i = 0; i < 7; i++){
-                if(elemento.match(Lettere[i])){
-                  
-                 int n = Lettere[i].length;
-                 Decodificatore newElemento = new Decodificatore(elemento);
-                    newElemento.rimCodice(Lettere[i].length);
-                    newElemento.addLettera(lettereChar[i]);
+        String[] codici = {"0","00","001","010","0010","0100","0110","0001"};
+        String[] lettere = {"A","B","C","D","E","F","G","H"};
 
+        //TODO:  mettere file e lettura, più veloce per prove
+        String input = "00100";
 
-                    if(newElemento.getLunghezzaCodice() == 0){ //Se non rimangono 0 o 1 aggiungo la iterazione al ArrayList dei risolto 
-                        Iterazioni.add(newElemento.getLetter());
-                    }
-                    else{   ///se ne rimangono lo aggiungo alla coda
-                        Albero.add(newElemento);
-                    }
-                    
-                }
-            }
-            elemento = Albero.poll(); //Fetch prossimo elemento
-        }while(elemento != null);         //Quando l'elemento seguente è null esco dal ciclo 
+        // AggiungO lettere
+        Albero alberoLettere = new Albero();
+        for (int i = 0; i < codici.length; i++) {
+            alberoLettere.inserisci(codici[i], lettere[i]);
+        }
 
-        int n =Iterazioni.size();
-
-        System.out.println(n);  //Stampo il numero di combinazioni trovate
-        for(int i = 0; i < n; i++){
-            System.out.println(Iterazioni.get(i)); //Stampo le combinazioni 
+        String[] risultati = {"ABA"};
+        System.out.println(risultati.length);
+        for (String risultato : risultati) {
+            System.out.println(risultato);
         }
     }
-    public static char[] leggiFile(String path){
-        char[] arrayCodice = {'0','0'};
 
-        return arrayCodice;
-        //TODO: scrivere metodo lettura file
-    }
-}    
+}
---
+++ Esercizio2Primo.java
@@ -0,0 +1,136 @@
+/*
+ * After some tries time complexity came outtoo high, next will try a different approach with dinamic programming
+ Also issues with reused variables etc, code is horrible, comments missing, names are ugly
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+class Decodificatore{
+    private  StringBuilder combinazione;
+    private Queue<Character> arrayCod;
+
+    public Decodificatore(){ //Con campi vuoti
+        arrayCod = new LinkedList<>();
+        this.combinazione = new StringBuilder();
+    }
+
+    public Decodificatore( char[] Input){
+        arrayCod = new LinkedList<>(); //Lo uso come FIFO, aggiungo tutti i valori, in seguito man mano che il codice binario viene interpretato li rimuovo 
+        for(char c : Input){
+            this.arrayCod.add(c);
+        }
+        this.combinazione = new StringBuilder(); ///Man mano che interpreto il codice aggiungo le lettere allo StringBuilder;
+        
+    }
+    public Decodificatore(Decodificatore other) {
+        this.combinazione = new StringBuilder(other.combinazione);
+        this.arrayCod = new LinkedList<>(other.arrayCod);
+    }
+    public void setCombinazione(String lettere){
+        this.combinazione.setLength(0); //Svuoto il Stringbuilder precedente
+        this.combinazione.append(lettere); //Aggiungo le lettere 
+    }
+    public void setCodice(char[] codice){
+        arrayCod.clear(); //Svuoto la coda
+        for(int i = 0; i < codice.length; i++){
+            arrayCod.add(codice[i]); //Aggiungo nuovo codice
+        }
+
+    }
+    public char[] getCod(){ 
+        Character[] arrayCharacter = arrayCod.toArray(new Character[0]);
+
+            char[] charArray = new char[arrayCharacter.length];
+        for (int i = 0; i < arrayCharacter.length; i++) {
+            charArray[i] = arrayCharacter[i]; // Unboxing del
+        }    
+        return charArray;
+    }
+    public void printCombinazione(){
+        System.out.println();
+       System.out.print(combinazione.toString());
+    }
+    public void addLettera(char lettera){
+        combinazione.append(lettera);
+    }
+    public String getLetter(){
+        return  combinazione.toString();
+    }
+    public void rimCodice(int n){   //metodo che dato un elemento char[] "es", elimina i primi es.length char dal codice   
+    for(int i = 0; i < n; i++){
+        this.arrayCod.poll();
+    }    
+}
+    public boolean match(char[] lettera){ //metodo che verifica se i caratteri in cima alla coda corrispondono a quelli del codice della lettera
+        Iterator<Character> iteratore = this.arrayCod.iterator(); //Definisco un iteratore per parsare la coda senza modificarla
+        for(int i = 0; i < lettera.length;i++){
+            if(iteratore.hasNext()){
+                if(iteratore.next() != lettera[i] ){
+                    return false;
+                }
+            }
+            else return false; //Qualora la parola finisse prima della lettera ritorno falso. 
+        }
+        return true;
+    }
+    public int getLunghezzaCodice(){
+        return this.arrayCod.size();
+    } 
+}
+
+public class Esercizio2Primo {
+    public static void main(String[] args) {
+        char[][] Lettere = {
+            {'0'}, {'0', '0'}, {'0', '0', '1'}, {'0', '1', '0'},
+            {'0', '0', '1', '0'}, {'0', '1', '0', '0'}, {'0', '1', '1', '0'}, {'0', '0', '0', '1'}
+        };
+        char[] lettereChar = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+        
+        char[] arrayCodice = leggiFile("args[0]"); //leggo il file con gli input
+        
+        Queue<Decodificatore> Albero = new LinkedList<>();  
+            Decodificatore origine = new Decodificatore(arrayCodice);
+        Albero.add(origine); //Inizializzo la coda
+        ArrayList<String> Iterazioni = new ArrayList<>(); //per tenere le iterazioni valide
+        
+        Decodificatore elemento = Albero.poll(); 
+        do{
+            for(int i = 0; i < 7; i++){
+                if(elemento.match(Lettere[i])){
+                  
+                 int n = Lettere[i].length;
+                 Decodificatore newElemento = new Decodificatore(elemento);
+                    newElemento.rimCodice(Lettere[i].length);
+                    newElemento.addLettera(lettereChar[i]);
+
+
+                    if(newElemento.getLunghezzaCodice() == 0){ //Se non rimangono 0 o 1 aggiungo la iterazione al ArrayList dei risolto 
+                        Iterazioni.add(newElemento.getLetter());
+                    }
+                    else{   ///se ne rimangono lo aggiungo alla coda
+                        Albero.add(newElemento);
+                    }
+                    
+                }
+            }
+            elemento = Albero.poll(); //Fetch prossimo elemento
+        }while(elemento != null);         //Quando l'elemento seguente è null esco dal ciclo 
+
+        int n =Iterazioni.size();
+
+        System.out.println(n);  //Stampo il numero di combinazioni trovate
+        for(int i = 0; i < n; i++){
+            System.out.println(Iterazioni.get(i)); //Stampo le combinazioni 
+        }
+    }
+    public static char[] leggiFile(String path){
+        char[] arrayCodice = {'0','0'};
+
+        return arrayCodice;
+        //TODO: scrivere metodo lettura file
+    }
+}    
